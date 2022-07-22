e5dda1aebe93e86bba84948214be45afabacd68a
GabbsCoding <gaby.ballester@gmail.com>
2022-07-22 20:06:11 +0200

    Hero detail + useMemo id and publisher

--- src/modules/heroes/components/HeroList.tsx
+++ src/modules/heroes/components/HeroList.tsx
@@ -1,13 +1,14 @@
 import { getHeroesByPublisher } from "../helpers";
 import { Publisher, IHero } from "../../../models/index";
 import { HeroCard } from "./HeroCard";
+import { useMemo } from "react";
 
 interface IHeroList {
   publisher: Publisher;
 }
 
 export const HeroList = ({ publisher }: IHeroList) => {
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
   return (
     <div className="row rows-cols-1 row-cols-md-3 g-3">
